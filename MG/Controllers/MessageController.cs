584f49b2d9144b942d5ae80c515dfddd5e067d07
ravikesana1998 <ramuawsdevops1@gmail.com>
2025-07-10 01:59:43 +0530

    Update MessageController.cs

--- MG/Controllers/MessageController.cs
+++ MG/Controllers/MessageController.cs
@@ -1,66 +1,64 @@
 namespace MG.Controllers
 {
-    [ApiController]               // ✅ Add this line
-    [Route("api/[controller]/[action]")]    // ✅ Add this line
 	public class MessageController : BaseController
 	{
-        private readonly IMessage_BLL _mailBll; // ✅ Add this line
+		IMessage_BLL _mailBll;
 
 		public MessageController(IMessage_BLL mailBll)
 		{
 			_mailBll = mailBll;
 		}
 
-		// [HttpGet("GetMessageByFolderAndId/{email}/{folder}/{messageId}")]   // ✅ Add this line
-		// public async Task<ActionResult> GetMessageByFolderAndId(string email, string folder, string messageId)
-		// {
-		// 	var response = await _mailBll.GetMessageByFolderAndId(email, folder, messageId);
-		// 	return Ok(response);
-		// }
+		[HttpGet("{email}/{folder}/{messageId}")]
+		public async Task<ActionResult> GetMessageByFolderAndId(string email, string folder, string messageId)
+		{
+			var response = await _mailBll.GetMessageByFolderAndId(email, folder, messageId);
+			return Ok(response);
+		}
 
-		[HttpGet("GetMessageById/{email}/{messageId}")]    // ✅ Add this line
+		[HttpGet("{email}/{messageId}")]
 		public async Task<ActionResult> GetMessagebyId(string email, string messageId)
 		{
 			var response = await _mailBll.GetMessagebyId(email, messageId);
 			return Ok(response);
 		}
 
-		[HttpGet("GetMessageHeaders/{email}/{messageId}")]   // ✅ Add this line
+		[HttpGet("{email}/{messageId}")]
 		public async Task<ActionResult> GetMessageHeaders(string email, string messageId)
 		{
 			var response = await _mailBll.GetMessageHeaders(email, messageId);
 			return Ok(response);
 		}
 
-		[HttpGet("DownloadMessage/{email}/{messageId}")]    // ✅ Add this line
+		[HttpGet("{email}/{messageId}")]
 		public async Task<ActionResult> DownloadMessage(string email, string messageId)
 		{
 			var response = await _mailBll.DownloadMessage(email, messageId);
 			return Ok(response);
 		}
 
-		[HttpGet("DownloadMessageasStream/{email}/{messageId}")]   // ✅ Add this line
+		[HttpGet("{email}/{messageId}")]
 		public async Task<ActionResult> DownloadMessageasStream(string email, string messageId)
 		{
 			var response = await _mailBll.DownloadMessageasStream(email, messageId);
 			return File(response, "message/rfc822", $"{messageId}.eml");
 		}
 
-		[HttpGet("GetMessagesbyFolder/{email}/{folderName}")]    // ✅ Add this line
+		[HttpGet("{email}/{folderName}")]
 		public async Task<ActionResult> GetMessagesbyFolder(string email, string folderName, string? filter, int pageNumber = 1, int pageSize = 10)
 		{
 			var response = await _mailBll.GetMessagesbyFolder(email, folderName, filter, pageNumber, pageSize);
 			return Ok(response);
 		}
 
-		[HttpGet("GetAttachmentsbyMessageId/{email}/{messageId}")]    // ✅ Add this line
+		[HttpGet("{email}/{messageId}")]
 		public async Task<ActionResult> GetAttachmentsbyMessageId(string email, string messageId)
 		{
 			var response = await _mailBll.GetAttachmentsbyMessageId(email, messageId);
 			return Ok(response);
 		}
 
-		[HttpGet("DownloadAttachmentbyId/{email}/{messageId}/{attachmentId}")]     // ✅ Add this line
+		[HttpGet("{email}/{messageId}/{attachmentId}")]
 		public async Task<ActionResult> DownloadAttachmentbyId(string email, string messageId, string attachmentId)
 		{
 			var attachment = await _mailBll.GetAttachmentbyId(email, messageId, attachmentId);
@@ -75,14 +73,14 @@ namespace MG.Controllers
 			}
 		}
 
-		[HttpGet("GetChildFolders/{email}/{parentFolderName}")]     // ✅ Add this line
+		[HttpGet("{email}/{parentFolderName}")]
 		public async Task<ActionResult> GetChildFolders(string email, string parentFolderName)
 		{
 			var response = await _mailBll.GetChildFolders(email, parentFolderName);
 			return Ok(response);
 		}
 
-		[HttpPost("MoveMessage")]  // ✅ Add this line
+		[HttpPost]
 		public async Task<ActionResult> MoveMessage(string email, string messageId, string destinationFolderId)
 		{
 			var response = await _mailBll.MoveMessage(email, messageId, destinationFolderId);
